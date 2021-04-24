ce77dda3762f2b2926f75f1c68d5d3ab47b9892c
Wolowitz-byte <74963986+Wolowitz-byte@users.noreply.github.com>
2021-04-24 19:48:47 +0530

    Add files via upload

---
+++ ProjectC-43-main/sketch.js
@@ -0,0 +1,36 @@
+//declaring all var
+var bg,bg2,form,system,code,security;
+var score=0;
+//preloading images
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+  bg2 = loadImage("treasure.jpg")
+}
+
+function setup() 
+{
+  //creating canvas
+  createCanvas(1000,500);
+  //creating new system and security
+  system = new System()
+  security = new Security()
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+  textSize(20);
+  fill("white");
+  text("Score: " + score, 450, 50);
+
+  if(score === 3) {
+    clear()
+    background(bg2)
+    fill("black")
+    textSize(40);
+    text("TREASURE UNLOCKED",250, 200);
+  }
+
+  drawSprites()
+}
